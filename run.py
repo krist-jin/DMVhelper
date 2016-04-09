d07bfe3a2f065fdf9d49cf6164fd989b801a803c
boj_vmware_vagrant_exp <krist333@126.com>
2016-04-08 17:16:00 -0700

    add return time

--- run.py
+++ run.py
@@ -1,45 +1,82 @@
 import requests
-from user_data import user_data
+from user_data import user_data_1, user_data_2
 from bs4 import BeautifulSoup
-from dateutil.parser import parse as parse_date
-from datetime import datetime
+from dateutil.parser import parse as parse_datetime
+import datetime
+import time
+import traceback
 
-DESIRED_DATE = datetime(2016, 4, 4).date()
+# DESIRED_DATE = datetime(2016, 4, 4).date()
 
 DMV_URL = "https://www.dmv.ca.gov/wasapp/foa/findDriveTest.do"
-
-headers = {
-	"Connection": "keep-alive",
-	"Content-Length": "553",
-	"Cache-Control": "max-age=0",
-	"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-	"Origin": "https://www.dmv.ca.gov",
-	"Upgrade-Insecure-Requests": "1",
-	"User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36",
-	"Content-Type": "application/x-www-form-urlencoded",
-	"Referer": "https://www.dmv.ca.gov/wasapp/foa/findDriveTest.do",
-	"Accept-Encoding": "gzip, deflate",
-	"Accept-Language": "en-US,en;q=0.8"
+DMV_OFFICES = {
+    "santa clara": ("632", 18), # 18 mins
+    "san jose": ("516", 24),  # 24 mins
+    # "fremont": ("644", 30), # 30 mins
+    # "san mateo": ("593", 40), # 40 mins
+    "redwood city": ("548", 28), # 28 mins
+    "los gatos": ("640", 19) # 19 mins
 }
 
-# send http post request
-raw_html = requests.post(DMV_URL, headers=headers, data=user_data).text
+HEADERS = {
+    "Connection": "keep-alive",
+    "Content-Length": "553",
+    "Cache-Control": "max-age=0",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Origin": "https://www.dmv.ca.gov",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Referer": "https://www.dmv.ca.gov/wasapp/foa/findDriveTest.do",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "en-US,en;q=0.8"
+}
+INTERVAL = 1 # run every 1 minute
 
-# parse html using bs4
-soup = BeautifulSoup(raw_html, 'html.parser')
+RETURN_TIME = datetime.time(10, 00) # need another 30 mins to arrive work
+EXAM_TIME = datetime.timedelta(hours=1)
 
-# extract result from soup result
-result_table = soup.find(id="app_content").table
-first_available_time = result_table.find_all("tr")[2].p.get_text()
-parsed_date = parse_date(first_available_time).date()
-if parsed_date == DESIRED_DATE:
-	print str(DESIRED_DATE) + " is available!"
-else:
-	print str(DESIRED_DATE) + " is not available!" 
-# print raw_html
-# print (soup.prettify())
-# print result_table.prettify()
-# print result_table.get_text()
-# print first_available_time
+def getReturnTime(app_datetime, dis_time):
+    return (app_datetime + EXAM_TIME + dis_time).time()
 
-# //*[@id="app_content"]/table
+def makeApp():
+    pass
+
+def getCurrentAppDatetime():
+    pass
+
+def main():
+    while True:
+        try:
+            print "\n******* %s *******" % str(datetime.datetime.now())
+            for office in DMV_OFFICES:
+                oid, dis_time_minutes = DMV_OFFICES[office]
+                dis_time = datetime.timedelta(minutes=dis_time_minutes)
+                # add office number to user_data
+                user_data_with_office = user_data_1+oid+user_data_2
+                # send http post request
+                raw_html = requests.post(DMV_URL, headers=HEADERS, data=user_data_with_office).text
+
+                # parse html using bs4
+                soup = BeautifulSoup(raw_html, 'html.parser')
+
+                # extract result from soup result
+                result_table = soup.find(id="app_content").table
+                first_available_time = result_table.find_all("tr")[2].p.get_text()
+                # parsed_date = parse_datetime(first_available_time).date()
+                parsed_datetime = parse_datetime(first_available_time)
+                returnTime = getReturnTime(parsed_datetime, dis_time)
+                # parsed_time = parsed_datetime.time()
+                if returnTime >= RETURN_TIME:
+                    print "%s: %s, return time: %s --- too late" % (office, first_available_time, returnTime)
+                else:
+                    print "%s: %s, return time: %s --- perfect" % (office, first_available_time, returnTime)
+        except Exception, e:
+            traceback.print_exc()
+            time.sleep(INTERVAL*60)
+            continue
+        else: # succeed
+            time.sleep(INTERVAL*60)
+
+if __name__ == '__main__':
+    main()
